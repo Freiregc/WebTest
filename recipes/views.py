078686d3d95b89e2592441d07f036df7a098af5b
Gabriel Freire <154226274+Freiregc@users.noreply.github.com>
2024-07-06 18:12:15 -0300

    upload 06/07

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request,'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request,'recipes/pages/home.html', context={'name': 'xD'})
 
 # Create your views here.
