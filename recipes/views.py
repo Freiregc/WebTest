cb1c6e4e167db7b9a3d3e017bc284a0b20426ade
Gabriel Freire <154226274+Freiregc@users.noreply.github.com>
2024-07-09 09:35:55 -0300

    last upload from 08/07/2024

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request,'recipes/pages/home.html', context={'name': 'xD'})
 
+def recipe(request, id):
+    return render(request,'recipes/pages/recipe-view.html', context={'name': 'xD'})
 # Create your views here.
